37bb17c1f06884d0e5624c74c9066b247537a11d
jwesfield <wesfieldj@gmail.com>
2020-04-30 00:46:52 +0300

    finished step1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,110 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { DatePicker } from 'antd';
+import { DatePicker, Divider, Statistic } from 'antd';
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
-import SlideInput from './components/SlideInput/SlideInput'
+import SlideInput from './components/SlideInput/SlideInput';
 function App() {
+
+  const [home, setHome] = useState(localStorage.getItem('home') || 0);
+  const [tax, setTax] = useState(localStorage.getItem('tax') || 0);
+  const [tax_percent, setTax_percent] = useState(localStorage.getItem('tax_percent') || 0)
+  const [agent, setAgent] = useState(localStorage.getItem('agent') || 0);
+  const [agent_percent, setAgent_percent] = useState(localStorage.getItem('agent_percent') || 0);
+  const [lawyer, setLawyer] = useState(localStorage.getItem('lawyer') || 0);
+  const [lawyer_percent, setLawyer_percent] = useState(localStorage.getItem('lawyer_percent') || 0);
+  const [other, setOther] = useState(localStorage.getItem('other') || 0);
+
+  const [downpayment, setDownpayment] = useState(
+    localStorage.getItem('downpayment') || 1
+  );
+  const [mortgage, setMortgage] = useState(
+    localStorage.getItem('mortgage') || 1
+  );
+  const [rate, setRate] = useState(localStorage.getItem('rate') || 1);
+  const [years, setYears] = useState(localStorage.getItem('years') || 1);
+
+function homeChanged(value){
+  setHome(value);
+  setTax(value * (tax_percent / 100))
+  setAgent(value * (agent_percent / 100))
+  setLawyer(value * (lawyer_percent / 100))
+}
+
   return (
     <div className="App">
-      <SlideInput />
+      <Divider type="horizontal" />
+      <SlideInput
+        boxes={[true, false]}
+        inputValue={home}
+        setInputValue={homeChanged}
+        min={1500000}
+        max={4000000}
+        step={50000}
+        storage={'home'}
+      />
+
+      <SlideInput
+        home={home}
+        boxes={[false, true]}
+        inputValue={tax}
+        setInputValue={setTax}
+        min={0}
+        max={4000000}
+        step={0.5}
+        storage={'tax'}
+        percent={tax_percent}
+        setPercent={setTax_percent}
+      />
+      <SlideInput
+        home={home}
+        boxes={[false, true]}
+        inputValue={agent}
+        setInputValue={setAgent}
+        min={0}
+        max={4000000}
+        step={0.5}
+        storage={'agent'}
+        percent={agent_percent}
+        setPercent={setAgent_percent}
+      />
+      <SlideInput
+        home={home}
+        boxes={[false, true]}
+        inputValue={lawyer}
+        setInputValue={setLawyer}
+        min={0}
+        max={4000000}
+        step={0.5}
+        storage={'lawyer'}
+        percent={lawyer_percent}
+        setPercent={setLawyer_percent}
+      />
+      <SlideInput
+        boxes={[true, false]}
+        inputValue={other}
+        setInputValue={setOther}
+        min={0}
+        max={200000}
+        step={1000}
+        storage={'other'}
+      />
+      <Statistic title="Total" value={getTotal(home,tax,lawyer,agent,other)} prefix={"$"} />
 
     </div>
   );
 }
 
+
+function getTotal(home,tax,lawyer,agent,other){
+  var res = 0 ;
+  try {
+    res = parseInt(home) + parseInt(tax) + parseInt(lawyer) + parseInt(agent) + parseInt(other)
+  } catch(e){
+
+  }
+return res;
+}
 export default App;
+
+//https://dev.to/yuribenjamin/how-to-deploy-react-app-in-github-pages-2a1f
--- src/components/SlideInput/SlideInput.jsx
+++ src/components/SlideInput/SlideInput.jsx
@@ -1,38 +1,84 @@
-import React, { useState } from 'react';
-import { Slider, InputNumber, Row, Col } from 'antd';
+import React, { useState, useEffect } from 'react';
+import './SlideInput.css';
+import { Slider, InputNumber, Row, Col, Typography, Divider } from 'antd';
+const { Text } = Typography;
 
-function SlideInput() {
-    
-    const [inputValue, setInputValue] = useState(1);
+function SlideInput(props) {
 
-    function onChange(value){
-        setInputValue(value);
-    }
+  function onChange(value) {
+    props.setInputValue(value);
+    localStorage.setItem(props.storage, value);
+  }
 
+  function onChangePercent(value) {
+    props.setPercent(value);
+    props.setInputValue(props.home * (value / 100));
+    localStorage.setItem(props.storage + '_percent', value);
+    localStorage.setItem(props.storage, props.home * (value / 100));
+  }
 
   return (
     <div>
       <Row>
-        <Col span={12}>
+        <Col span={4}>
+          <Text strong>{props.storage}</Text>
+        </Col>
+        <Col span={8}>
           <Slider
-            min={1}
-            max={20}
-            onChange={onChange}
-            value={typeof inputValue === 'number' ? inputValue : 0}
+            min={props.percent ? 0 : props.min}
+            max={props.percent ? 10 : props.max}
+            step={props.percent ? 0.5 : props.step}
+            onChange={props.percent ? onChangePercent : onChange}
+            value={props.percent ? props.percent : props.inputValue}
           />
         </Col>
-        <Col span={4}>
+        <Col span={3}>
           <InputNumber
-            min={1}
-            max={20}
+            className="inputCurrency"
+            min={props.min || 0}
+            max={props.max || 100}
+            step={props.step || 1}
             style={{ margin: '0 16px' }}
-            value={inputValue}
+            value={props.inputValue}
             onChange={onChange}
+            formatter={cFormatter}
+            parser={cParser}
+            disabled={!props.boxes[0]}
           />
+        </Col>
+        <Col span={3}>
+          {props.boxes[1] == true ? (
+            <InputNumber
+              min={0}
+              max={10}
+              step={0.5}
+              style={{ margin: '0 16px' }}
+              value={props.percent}
+              onChange={onChangePercent}
+              formatter={pFormatter}
+              parser={pParser}
+            />
+          ) : null}
         </Col>
       </Row>
+      <Divider type="horizontal" />
     </div>
   );
+}
+
+function pFormatter(value) {
+  return `${value}%`;
+}
+function pParser(value) {
+  return value.replace('%', '');
+}
+
+function cFormatter(value) {
+  return `₪ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+function cParser(value) {
+  return value.replace(/\₪\s?|(,*)/g, '');
+  //return value.replace('₪', '');
 }
 
 export default SlideInput;
